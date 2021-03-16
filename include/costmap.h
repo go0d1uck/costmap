2e7d38d763c10b01d915129bd39f8da122b5460b
go0d1uck <antraume@gmail.com>
2021-03-17 00:05:50 +0800

    add inflation

--- include/costmap.h
+++ include/costmap.h
@@ -1,10 +1,13 @@
 #ifndef COSTMAP_H
 #define COSTMAP_H
 #include "layer.h"
+#include <algorithm>
 #include <cmath>
+#include <cstring>
 #include <glog/logging.h>
 #include <map>
 #include <memory>
+#include <queue>
 #include <string>
 #include <thread>
 #include <unordered_map>
@@ -20,6 +23,18 @@ class Costmap {
   std::map<std::string, std::shared_ptr<Layer>> plugins_;
   /** @brief the layer after layered */
   std::shared_ptr<Layer> pLayered_;
+
+  /** @brief useful for inflation */
+  struct Cell {
+    int mx, my, src_x, src_y;
+    Cell(int x, int y, int tx, int ty)
+        : mx(x)
+        , my(y)
+        , src_x(tx)
+        , src_y(ty)
+    {
+    }
+  };
 
   public:
   Costmap(double robot_x, double robot_y, double robot_yaw, double size);
@@ -41,7 +56,46 @@ class Costmap {
     }
     return cost;
   }
-  bool Inflation();
+  inline unsigned char GetCost(int mx, int my, int src_x, int src_y)
+  {
+    unsigned int dx = std::abs(mx - src_x);
+    unsigned int dy = std::abs(my - src_y);
+    return ComputeCost(std::hypot(dx, dy));
+  }
+  void Inflation(std::vector<std::vector<unsigned char>>& return_costmap)
+  {
+    int limit = pLayered_->getSize() / pLayered_->getResolution();
+    int grid_inscribed_dis = INSCRIBED_RADIUS / pLayered_->getResolution();
+    limit++;
+    grid_inscribed_dis++;
+    bool seen[limit][limit];
+    std::fill(seen[0], seen[0] + limit * limit, false);
+    std::queue<Cell> q; /** @brief need inflation queue */
+    std::vector<std::vector<bool>> local_map = pLayered_->getMap();
+    for (int i = 0; i < limit; i++) {
+      for (int j = 0; j < limit; j++) {
+        if (local_map[i][j]) {
+          return_costmap[i][j] = LETHAL_OBSTACLE;
+          q.push(Cell(i, j, i, j));
+          seen[i][j] = true;
+        }
+      }
+    }
+    while (!q.empty()) {
+      const Cell& cell_data = q.front();
+      q.pop();
+      if (cell_data.mx > 0)
+        q.push(Cell(cell_data.mx - 1, cell_data.my, cell_data.src_x, cell_data.src_y));
+      if (cell_data.my > 0)
+        q.push(Cell(cell_data.mx, cell_data.my - 1, cell_data.src_x, cell_data.src_y));
+      if (cell_data.mx < limit - 1)
+        q.push(Cell(cell_data.mx + 1, cell_data.my, cell_data.src_x, cell_data.src_y));
+      if (cell_data.my < limit - 1)
+        q.push(Cell(cell_data.mx, cell_data.my + 1, cell_data.src_x, cell_data.src_y));
+      /** @brief choose big cost */
+      return_costmap[cell_data.mx][cell_data.my] = std::max(return_costmap[cell_data.mx][cell_data.my], GetCost(cell_data.mx, cell_data.my, cell_data.src_x, cell_data.src_y));
+    }
+  }
 };
 }
 
